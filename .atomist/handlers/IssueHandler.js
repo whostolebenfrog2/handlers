b9f54fec28891d94a00f3d4cd475192cfaee492f
Christian Dupuis <cd@atomist.com>
2016-12-09 18:37:26 +0100

    Remove JS

--- .atomist/handlers/IssueHandler.js
+++
@@ -1,9 +0,0 @@
-atomist.on("/issue[.number()=1]", function (m) {
-    var issue = m.root();
-    console.log("We got here! The issue is " + issue);
-    atomist.messageBuilder().say("Nice, I don't know what I'm doing but it works").send();
-    atomist.messageBuilder().regarding(issue).send();
-    if (issue.resolvedBy() == null) {
-        console.log("ResolvedBy is null");
-    }
-});
--- .atomist/handlers/PushHandler.js
+++
@@ -1,4 +0,0 @@
-atomist.on("/push", function (m) {
-    var push = m.root();
-    atomist.messageBuilder().regarding(push).send();
-});
