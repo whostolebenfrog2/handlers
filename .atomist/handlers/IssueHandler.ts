16773124ff9874518c07f9eab2388bd173e1195c
Jim Clark <slimslenderslacks@gmail.com>
2016-12-12 16:40:32 -0800

    distinguish between open and closed issues

--- .atomist/handlers/IssueHandler.ts
+++ .atomist/handlers/IssueHandler.ts
@@ -1,6 +1,12 @@
 declare var atomist
 
-atomist.on("/issue", m => {
+atomist.on("/issue[.state()='open']", m => {
    let issue = m.root()
+   atomist.messageBuilder().regarding(issue).send()
+})
+
+atomist.on("/issue[.state()='closed']", m => {
+   let issue = m.root()
+   atomist.messageBuilder().say("this next message is for a closed event").send()
    atomist.messageBuilder().regarding(issue).send()
 })
