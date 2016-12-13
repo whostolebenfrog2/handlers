c5db6a2012337079977fd7a94d9e1fce7c5c8a60
Christian Dupuis <cd@atomist.com>
2016-12-13 01:29:50 +0100

    2 commit

--- .atomist/handlers/PushHandler.ts
+++ .atomist/handlers/PushHandler.ts
@@ -1,10 +1,10 @@
 declare var atomist
-
+ 
 atomist.on("/push", m => {
    let push = m.root()
    atomist.messageBuilder().regarding(push).send()
 })
- 
+
 atomist.on("/commit", m => {
    let push = m.root()
    atomist.messageBuilder().regarding(push).send()
