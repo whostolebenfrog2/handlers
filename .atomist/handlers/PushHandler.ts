619e7261c490e181217ffc5ad2d790471f074d95
Christian Dupuis <cd@atomist.com>
2016-12-19 16:14:10 +0100

    Use Generics to make compiler happy

--- .atomist/handlers/PushHandler.ts
+++ .atomist/handlers/PushHandler.ts
@@ -2,12 +2,12 @@ import {Atomist} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
-atomist.on("/push", m => {
+atomist.on<TreeNode, TreeNode>("/push", m => {
    let push = m.root()
    atomist.messageBuilder().regarding(push).send()
 })
 
-atomist.on("/commit", m => {
+atomist.on<TreeNode, TreeNode>("/commit", m => {
    let push = m.root()
    atomist.messageBuilder().regarding(push).send()
 })
