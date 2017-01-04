20b32c09098d59811fd6885b71937675f41fb478
Tanya Romankova <tanya@atomist.com>
2017-01-04 09:57:55 -0600

    Remove commit handler and fix handler names

--- .atomist/handlers/BuildHandler.ts
+++ .atomist/handlers/BuildHandler.ts
@@ -2,14 +2,7 @@ import {Atomist} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
-atomist.on<TreeNode, TreeNode>("/push", m => {
-   let push = m.root()
-   let message = atomist.messageBuilder().regarding(push)
-   message.withAction(message.actionRegistry().findByName("RestartTravisBuild"))
-   message.send()
-})
-
-atomist.on<TreeNode, TreeNode>("/commit", m => {
+atomist.on<TreeNode, TreeNode>("/build", m => {
    let push = m.root()
    atomist.messageBuilder().regarding(push).send()
 })
--- .atomist/handlers/PushHandler.ts
+++ .atomist/handlers/PushHandler.ts
@@ -2,7 +2,9 @@ import {Atomist} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
-atomist.on<TreeNode, TreeNode>("/build", m => {
+atomist.on<TreeNode, TreeNode>("/push", m => {
    let push = m.root()
-   atomist.messageBuilder().regarding(push).send()
+   let message = atomist.messageBuilder().regarding(push)
+   message.withAction(message.actionRegistry().findByName("RestartTravisBuild"))
+   message.send()
 })
