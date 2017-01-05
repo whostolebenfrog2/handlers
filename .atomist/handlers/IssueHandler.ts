ae2362b767d35e8330dc2a0a28cef3cfc722a6c3
Christian Dupuis <cd@atomist.com>
2017-01-05 23:22:52 +0100

    Updates for rug 0.8.0

--- .atomist/handlers/IssueHandler.ts
+++ .atomist/handlers/IssueHandler.ts
@@ -5,13 +5,13 @@ declare var atomist: Atomist
 atomist.on<TreeNode, TreeNode>("/issue[.state()='open']", m => {
    let issue = m.root()
    let message = atomist.messageBuilder().regarding(issue)
-   message.withAction(message.actionRegistry().findByName("HelloWorld"))
+   message.withAction(message.actionRegistry().findByName("Assign"))
    message.send()
 })
 
 atomist.on<TreeNode, TreeNode>("/issue[.state()='closed']", m => {
    let issue = m.root()
    let message = atomist.messageBuilder().regarding(issue)
-   message.withAction(message.actionRegistry().findByName("HelloWorld"))
+   message.withAction(message.actionRegistry().findByName("Reopen"))
    message.send()
 })
