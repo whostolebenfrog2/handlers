09c640d3dabeef3283badea7d88a74e1aace03a5
Christian Dupuis <cd@atomist.com>
2017-01-10 15:38:33 +0100

    Add createRelease action to Build

--- .atomist/handlers/BuildHandler.ts
+++ .atomist/handlers/BuildHandler.ts
@@ -3,6 +3,10 @@ import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
 atomist.on<TreeNode, TreeNode>("/build", m => {
-   let push = m.root()
-   atomist.messageBuilder().regarding(push).send()
+   let build = m.root() as any
+   let message = atomist.messageBuilder().regarding(build)
+   if (build.status() == "Passed" || build.status() == "Fixed") {
+     message.withAction(message.actionRegistry().findByName("CreateRelease"))
+   }
+   message.send()
 })
