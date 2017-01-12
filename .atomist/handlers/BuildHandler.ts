bfb3502b61aac7ad13fc7441af55961bedb58a66
Christian Dupuis <cd@atomist.com>
2017-01-12 17:30:10 +0100

    Some refactoring

--- .atomist/handlers/BuildHandler.ts
+++ .atomist/handlers/BuildHandler.ts
@@ -1,4 +1,4 @@
-import {Atomist} from '@atomist/rug/operations/Handler'
+import {Atomist, MessageBuilder} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
@@ -7,20 +7,17 @@ atomist.on<TreeNode, TreeNode>("/build", m => {
    let mb = atomist.messageBuilder()
    let message = mb.regarding(build)
    let repo = "`" + build.repo().owner() + "/" + build.repo().name() + "`"
+   let commit = "`" + build.commit().sha() + "`"
 
    // TODO split this into two handlers with proper tree expressions with predicates
    if (build.status() == "Passed" || build.status() == "Fixed") {
      message.withAction(message.actionRegistry().findByName("CreateRelease"))
-     if (build.status() == "Fixed" && build.commit().committer().person() != null) {
-        mb.say(`Travis CI build of repo ${repo} is now fixed`).on(build.commit().committer().person().chatIdentity().chatId()).send()
+     if (build.status() == "Fixed") {
+       sendDirectMessage(build, `Travis CI build of repo ${repo} is now fixed`, mb)
      }
    }
    else if (build.status() == "Failed" || build.status() == "Broken" || build.status() == "Still Failing") {
-     if (build.commit().committer().person() != null) {
-        let repo = "`" + build.repo().owner() + "/" + build.repo().name() + "`"
-        let commit = "`" + build.commit().sha() + "`"
-        mb.say(`Travis CI build of repo ${repo} failed after your last commit ${commit}: ${build.build_url()}`).on(build.commit().committer().person().chatIdentity().chatId()).send()
-     }
+     sendDirectMessage(build, `Travis CI build of repo ${repo} failed after your last commit ${commit}: ${build.build_url()}`, mb)
 
      // Attach restart action
      let restart = message.actionRegistry().findByName("RestartBuild")
@@ -33,3 +30,9 @@ atomist.on<TreeNode, TreeNode>("/build", m => {
 
    message.send()
 })
+
+function sendDirectMessage(build: any, message: string, mb: MessageBuilder) {
+  if (build.commit().committer().person() != null) {
+      mb.say(message).on(build.commit().committer().person().chatIdentity().chatId()).send()
+  }
+}
