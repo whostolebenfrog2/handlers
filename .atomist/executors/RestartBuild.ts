7393c78738ea0d143e104ca750d4248eb7e59d71
Christian Dupuis <cd@atomist.com>
2017-01-12 17:58:57 +0100

    Debug restart

--- .atomist/executors/RestartBuild.ts
+++ .atomist/executors/RestartBuild.ts
@@ -21,13 +21,14 @@ var restartBuild: Executor = {
         // TODO proper patterns and validation
         { name: "build_id", description: "Build Number", pattern: "^.*$", maxLength: 100, required: true },
         { name: "build_no", description: "Build No", pattern: "^.*$", maxLength: 100, required: false },
+        { name: "org", description: "Travis org", pattern: "^(.org|.com)$", maxLength: 100, required: false, default:".org" },
         { name: "owner", description: "GitHub Owner", pattern: "^.*$", maxLength: 100, required: true, displayable: false, tags: ["atomist/owner"] },
         { name: "repo", description: "GitHub Repo", pattern: "^.*$", maxLength: 100, required: true, displayable: false, tags: ["atomist/repository"] },
-        { name: "org", description: "Travis org", pattern: "^(.org|.com)$", maxLength: 100, required: false, default:".org" },
         // TODO marking it required: false will prevent the bot to ask for it
         { name: "token", description: "GitHub Token", pattern: "^.*$", maxLength: 100, required: false, displayable: false, tags: ["atomist/user_token"] }
     ],
     execute(services: Services, p: Parameters): Result {
+        console.log(p)
         let _services: any = services
         let travisService = _services.travis() as TravisService
         let status = travisService.restartBuild(p.build_id, p.org, p.token)
