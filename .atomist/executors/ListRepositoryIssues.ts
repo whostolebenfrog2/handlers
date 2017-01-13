6d0be541cb2e53abad90278e960f453432a8d349
Christian Dupuis <cd@atomist.com>
2017-01-14 00:14:18 +0100

    New open issues service channel command

--- .atomist/executors/ListRepositoryIssues.ts
+++ .atomist/executors/ListRepositoryIssues.ts
@@ -5,17 +5,19 @@ import { Result, Status, Parameter } from "@atomist/rug/operations/RugOperation"
 import { GitHubService, Issue } from "@atomist/github/core/Core"
 
 interface Parameters {
-    days: number
+    owner: string
+    repo: string
     token: string
 }
 
-var listIssues: Executor = {
-    description: "List user's GitHub issues",
-    name: "ListIssues",
-    tags: ["atomist/intent=list issues"],
+var listRepositoryIssues: Executor = {
+    description: "List repository GitHub issues",
+    name: "ListRepositoryIssues",
+    tags: ["atomist/intent=open issues"],
     parameters: [
         // TODO proper patterns and validation
-        { name: "days", description: "Days", pattern: "^.*$", maxLength: 100, required: false, default: "1" },
+        { name: "owner", description: "GitHub Owner", pattern: "^.*$", maxLength: 100, required: true, displayable: false, tags: ["atomist/owner"] },
+        { name: "repo", description: "GitHub Repo", pattern: "^.*$", maxLength: 100, required: true, displayable: false, tags: ["atomist/repository"] },
         // TODO marking it required: false will prevent the bot to ask for it
         { name: "token", description: "GitHub Token", pattern: "^.*$", maxLength: 100, required: false, displayable: false, tags: ["atomist/user_token"] }
     ],
@@ -23,33 +25,22 @@ var listIssues: Executor = {
 
         let _services: any = services
         let githubService = _services.github() as GitHubService
-        let issues: Issue[] = githubService.listIssues(p.days, p.token)
+        let issues: Issue[] = githubService.listIssues(p.owner, p.repo, p.token)
 
         if (issues.length > 0) {
             let attachments = `{"attachments": [` + issues.map(i => {
-                if (i.state() == "closed") {
-                    return `{
-                  "fallback": "#${i.number()}: ${i.title()}",
-                  "author_icon": "http://images.atomist.com/rug/issue-closed.png",
-                  "color": "#bd2c00",
-                  "author_link": "${i.issueUrl()}",
-                  "author_name": "#${i.number()}: ${i.title()}"
-               }`
-                }
-                else {
-                    return `{
+                return `{
                 "fallback": "#${i.number()}: ${i.title()}",
                 "author_icon": "http://images.atomist.com/rug/issue-open.png",
                 "color": "#6cc644",
                 "author_link": "${i.issueUrl()}",
                 "author_name": "#${i.number()}: ${i.title()}"
              }`
-                }
             }).join(",") + "]}"
             _services.messageBuilder().say(attachments).send()
         }
         else {
-            _services.messageBuilder().say(`Looks like you really didn't crush it. No issues found for the last ${p.days} day(s)`).send()
+            _services.messageBuilder().say(`No open issues on ${p.owner}/${p.repo}`).send()
         }
         return new Result(Status.Success, "OK")
     }
