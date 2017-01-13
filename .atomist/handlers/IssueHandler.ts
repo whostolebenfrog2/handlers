a485d5920d1f67da97399e777663d5f3bc4df084
Tanya Romankova <tanya@atomist.com>
2017-01-13 16:17:11 -0600

    Tweak button titles

--- .atomist/handlers/IssueHandler.ts
+++ .atomist/handlers/IssueHandler.ts
@@ -6,21 +6,21 @@ atomist.on<TreeNode, TreeNode>("/issue[.state()='open']", m => {
    let issue = m.root() as any
    let message = atomist.messageBuilder().regarding(issue)
 
-   let assign = message.actionRegistry().findByName("AssignIssue|Assign")
+   let assign = message.actionRegistry().findByName("AssignIssue|Assign Issue")
    assign = message.actionRegistry().bindParameter(assign, "number", issue.number())
    message.withAction(assign)
 
-   let bug = message.actionRegistry().findByName("LabelIssue|Bug")
+   let bug = message.actionRegistry().findByName("LabelIssue|Add Label: Bug")
    bug = message.actionRegistry().bindParameter(bug, "number", issue.number())
    bug = message.actionRegistry().bindParameter(bug, "label", "bug")
    message.withAction(bug)
 
-   let enhancement = message.actionRegistry().findByName("LabelIssue|Enhancement")
+   let enhancement = message.actionRegistry().findByName("LabelIssue|Add Label: Enhancement")
    enhancement = message.actionRegistry().bindParameter(enhancement, "number", issue.number())
    enhancement = message.actionRegistry().bindParameter(enhancement, "label", "enhancement")
    message.withAction(enhancement)
 
-   let close = message.actionRegistry().findByName("CloseIssue|Close")
+   let close = message.actionRegistry().findByName("CloseIssue|Close Issue")
    close = message.actionRegistry().bindParameter(close, "number", issue.number())
    message.withAction(close)
 
@@ -35,7 +35,7 @@ atomist.on<TreeNode, TreeNode>("/issue[.state()='closed']", m => {
    let issue = m.root() as any
    let message = atomist.messageBuilder().regarding(issue)
 
-   let reopen = message.actionRegistry().findByName("ReopenIssue|Reopen")
+   let reopen = message.actionRegistry().findByName("ReopenIssue|Reopen Issue")
    reopen = message.actionRegistry().bindParameter(reopen, "number", issue.number())
    message.withAction(reopen)
 
