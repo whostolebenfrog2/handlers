ef4fd926b855cd46ef81225f825b4845b06c925e
Christian Dupuis <cd@atomist.com>
2017-01-16 17:52:01 +0100

    Remove duplication, TS is a proper languange

--- .atomist/handlers/IssueHandler.ts
+++ .atomist/handlers/IssueHandler.ts
@@ -1,4 +1,4 @@
-import {Atomist} from '@atomist/rug/operations/Handler'
+import {Atomist, Message} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
@@ -6,49 +6,18 @@ atomist.on<TreeNode, TreeNode>("/issue[.state()='open']", m => {
    let issue = m.root() as any
    let message = atomist.messageBuilder().regarding(issue)
 
-   let assign = message.actionRegistry().findByName("AssignIssue|Assign")
-   assign = message.actionRegistry().bindParameter(assign, "number", issue.number())
-   assign = message.actionRegistry().bindParameter(assign, "repo", issue.repo().name())
-   assign = message.actionRegistry().bindParameter(assign, "owner", issue.repo().owner())
-   message.withAction(assign)
-
-   let bug = message.actionRegistry().findByName("LabelIssue|Label as Bug")
-   bug = message.actionRegistry().bindParameter(bug, "number", issue.number())
-   bug = message.actionRegistry().bindParameter(bug, "label", "bug")
-   bug = message.actionRegistry().bindParameter(bug, "repo", issue.repo().name())
-   bug = message.actionRegistry().bindParameter(bug, "owner", issue.repo().owner())
-   message.withAction(bug)
-
-   let enhancement = message.actionRegistry().findByName("LabelIssue|Label as Enhancement")
-   enhancement = message.actionRegistry().bindParameter(enhancement, "number", issue.number())
-   enhancement = message.actionRegistry().bindParameter(enhancement, "label", "enhancement")
-   enhancement = message.actionRegistry().bindParameter(enhancement, "repo", issue.repo().name())
-   enhancement = message.actionRegistry().bindParameter(enhancement, "owner", issue.repo().owner())
-   message.withAction(enhancement)
-
-   let close = message.actionRegistry().findByName("CloseIssue|Close")
-   close = message.actionRegistry().bindParameter(close, "number", issue.number())
-   close = message.actionRegistry().bindParameter(close, "repo", issue.repo().name())
-   close = message.actionRegistry().bindParameter(close, "owner", issue.repo().owner())
-   message.withAction(close)
-
-   let comment = message.actionRegistry().findByName("CommentIssue|Comment")
-   comment = message.actionRegistry().bindParameter(comment, "number", issue.number())
-   comment = message.actionRegistry().bindParameter(comment, "repo", issue.repo().name())
-   comment = message.actionRegistry().bindParameter(comment, "owner", issue.repo().owner())
-   message.withAction(comment)
-
-   message.send()
+   bindIssueActions(message, issue.number(), issue.repo().owner(), issue.repo().name()).send()
 })
 
 atomist.on<TreeNode, TreeNode>("/issue[.state()='closed']", m => {
    let issue = m.root() as any
    let message = atomist.messageBuilder().regarding(issue)
+   let registry = message.actionRegistry()
 
-   let reopen = message.actionRegistry().findByName("ReopenIssue|Reopen")
-   reopen = message.actionRegistry().bindParameter(reopen, "number", issue.number())
-   reopen = message.actionRegistry().bindParameter(reopen, "repo", issue.repo().name())
-   reopen = message.actionRegistry().bindParameter(reopen, "owner", issue.repo().owner())
+   let reopen = registry.findByName("ReopenIssue|Reopen")
+   reopen = registry.bindParameter(reopen, "number", issue.number())
+   reopen = registry.bindParameter(reopen, "repo", issue.repo().name())
+   reopen = registry.bindParameter(reopen, "owner", issue.repo().owner())
    message.withAction(reopen)
 
    message.send()
@@ -58,37 +27,44 @@ atomist.on<TreeNode, TreeNode>("/comment", m => {
    let issueComment = m.root() as any
    let message = atomist.messageBuilder().regarding(issueComment)
 
-   let assign = message.actionRegistry().findByName("AssignIssue|Assign")
-   assign = message.actionRegistry().bindParameter(assign, "number", issueComment.on().number())
-   assign = message.actionRegistry().bindParameter(assign, "repo", issueComment.repo().name())
-   assign = message.actionRegistry().bindParameter(assign, "owner", issueComment.repo().owner())
-   message.withAction(assign)
-
-   let bug = message.actionRegistry().findByName("LabelIssue|Label as Bug")
-   bug = message.actionRegistry().bindParameter(bug, "number", issueComment.on().number())
-   bug = message.actionRegistry().bindParameter(bug, "label", "bug")
-   bug = message.actionRegistry().bindParameter(bug, "repo", issueComment.repo().name())
-   bug = message.actionRegistry().bindParameter(bug, "owner", issueComment.repo().owner())
-   message.withAction(bug)
-
-   let enhancement = message.actionRegistry().findByName("LabelIssue|Label as Enhancement")
-   enhancement = message.actionRegistry().bindParameter(enhancement, "number", issueComment.on().number())
-   enhancement = message.actionRegistry().bindParameter(enhancement, "label", "enhancement")
-   enhancement = message.actionRegistry().bindParameter(enhancement, "repo", issueComment.repo().name())
-   enhancement = message.actionRegistry().bindParameter(enhancement, "owner", issueComment.repo().owner())
-   message.withAction(enhancement)
-
-   let close = message.actionRegistry().findByName("CloseIssue|Close")
-   close = message.actionRegistry().bindParameter(close, "number", issueComment.on().number())
-   close = message.actionRegistry().bindParameter(close, "repo", issueComment.repo().name())
-   close = message.actionRegistry().bindParameter(close, "owner", issueComment.repo().owner())
-   message.withAction(close)
-
-   let comment = message.actionRegistry().findByName("CommentIssue|Comment")
-   comment = message.actionRegistry().bindParameter(comment, "number", issueComment.on().number())
-   comment = message.actionRegistry().bindParameter(comment, "repo", issueComment.repo().name())
-   comment = message.actionRegistry().bindParameter(comment, "owner", issueComment.repo().owner())
-   message.withAction(comment)
-
-   message.send()
+   bindIssueActions(message, issueComment.on().number(), issueComment.repo().owner(), issueComment.repo().name()).send()
 })
+
+function bindIssueActions(message: Message, number: number, owner: string, repo: string): Message {
+  let registry = message.actionRegistry()
+
+  let assign = registry.findByName("AssignIssue|Assign")
+  assign = registry.bindParameter(assign, "number", number)
+  assign = registry.bindParameter(assign, "repo", repo)
+  assign = registry.bindParameter(assign, "owner", owner)
+  message.withAction(assign)
+
+  let bug = registry.findByName("LabelIssue|Label as Bug")
+  bug = registry.bindParameter(bug, "number", number)
+  bug = registry.bindParameter(bug, "label", "bug")
+  bug = registry.bindParameter(bug, "repo", repo)
+  bug = registry.bindParameter(bug, "owner", owner)
+  message.withAction(bug)
+
+  let enhancement = registry.findByName("LabelIssue|Label as Enhancement")
+  enhancement = registry.bindParameter(enhancement, "number", number)
+  enhancement = registry.bindParameter(enhancement, "label", "enhancement")
+  enhancement = registry.bindParameter(enhancement, "repo", repo)
+  enhancement = registry.bindParameter(enhancement, "owner", owner)
+  message.withAction(enhancement)
+
+  let close = registry.findByName("CloseIssue|Close")
+  close = registry.bindParameter(close, "number", number)
+  close = registry.bindParameter(close, "repo", repo)
+  close = registry.bindParameter(close, "owner", owner)
+  message.withAction(close)
+
+  let comment = registry.findByName("CommentIssue|Comment")
+  comment = registry.bindParameter(comment, "number", number)
+  comment = registry.bindParameter(comment, "repo", repo)
+  comment = registry.bindParameter(comment, "owner", owner)
+  message.withAction(comment)
+
+  return message
+
+}
