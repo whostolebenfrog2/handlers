42ed4529c019de962b2cd5650dbc5a936c474e9b
Jessica Kerr <jessitron@gmail.com>
2017-01-30 17:01:58 -0800

    Let's try sending DMs on issue comments

---
+++ .atomist/handlers/IssueCommentHandler.ts
@@ -0,0 +1,17 @@
+import {Atomist, MessageBuilder} from '@atomist/rug/operations/Handler'
+import {TreeNode} from '@atomist/rug/tree/PathExpression'
+declare var atomist: Atomist
+
+atomist.on<TreeNode, TreeNode>("/comment", m => {
+   let comment = m.root() as any
+   let mb = atomist.messageBuilder()
+   sendDirectMessage(
+     comment, `${comment.by().login()} commented on #${comment.on().number()}:${comment.on().title()}\n> ${comment.body()}`, mb)
+})
+
+function sendDirectMessage(comment: any, message: string, mb: MessageBuilder) {
+  let who = comment.on().by().person()
+  if (who != null) {
+      mb.say(message).on(who.chatIdentity().chatId()).send()
+  }
+}
