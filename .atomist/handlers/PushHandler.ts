ccdb74f71c16768bb3e9333235b182e9c57d3df4
Ben Griffiths <mrbengriffiths@googlemail.com>
2017-02-15 15:21:42 +0000

    Ignore types on push

--- .atomist/handlers/PushHandler.ts
+++ .atomist/handlers/PushHandler.ts
@@ -3,7 +3,7 @@ import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
 atomist.on<TreeNode, TreeNode>("/Push[/on::Repo()/channel::ChatChannel()][/contains::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]", m => {
-   let push = m.root()
+   let push = m.root() as Any
    let cid = "commit_event/" + push.on().owner() + "/" + push.on().name() + "/" + push.after()
 
    let message = atomist.messageBuilder().regarding(push).withCorrelationId(cid).send()
