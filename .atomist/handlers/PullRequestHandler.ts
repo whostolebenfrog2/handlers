227b2930aa4c0dd9977d9a8e65d3b58206cce156
Ben Griffiths <ben@atomist.com>
2017-02-23 12:00:51 +0000

    Add build handler for PR, update PR handler

--- .atomist/handlers/PullRequestHandler.ts
+++ .atomist/handlers/PullRequestHandler.ts
@@ -2,26 +2,40 @@ import {Atomist} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
-atomist.on<TreeNode, TreeNode>("/PullRequest()[/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?][/mergedBy::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]?[/on::Repo()/channel::ChatChannel()][/head::Commit()/contains::Push()[/on::Repo()][/contains::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]]", m => {
-   let pr = m.root() as any
+atomist.on<TreeNode, TreeNode>(
+    `/PullRequest()
+        [/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
+        [/mergedBy::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]?
+        [/contains::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
+        [/triggeredBy::Build()/on::Repo()]
+        [/on::Repo()/channel::ChatChannel()]`, m => {
 
+   let pr = m.root() as any
+ 
    // temp workaound to issue in path expressions looking up values on JsonBackedTreeNodes
    if (pr.state() != "open") {
      return
    }
-
+ 
    let message = atomist.messageBuilder().regarding(pr)
-
+ 
    let merge = message.actionRegistry().findByName("MergePullRequest|Merge")
    merge = message.actionRegistry().bindParameter(merge, "number", pr.number())
    message.withAction(merge)
-   
-   let cid = "pr_event/" + pr.on().owner() + "/" + pr.on().name() + "/" + pr.head().sha()
-
+ 
+   let cid = "pr_event/" + pr.on().owner() + "/" + pr.on().name() + "/" + pr.number()
+ 
    message.withCorrelationId(cid).send()
 })
 
-atomist.on<TreeNode, TreeNode>("/PullRequest()[/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?][/mergedBy::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]?[/on::Repo()/channel::ChatChannel()][/head::Commit()/contains::Push()[/on::Repo()][/contains::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]]", m => {
+atomist.on<TreeNode, TreeNode>(
+    `/PullRequest()
+        [/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
+        [/mergedBy::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]?
+        [/contains::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
+        [/triggeredBy::Build()/on::Repo()]
+        [/on::Repo()/channel::ChatChannel()]`, m => {
+
    let pr = m.root() as any
 
    // temp workaound to issue in path expressions looking up values on JsonBackedTreeNodes
@@ -31,7 +45,7 @@ atomist.on<TreeNode, TreeNode>("/PullRequest()[/author::GitHubId()[/hasGithubIde
 
    let message = atomist.messageBuilder().regarding(pr)
    
-   let cid = "pr_event/" + pr.on().owner() + "/" + pr.on().name() + "/" + pr.head().sha()
+   let cid = "pr_event/" + pr.on().owner() + "/" + pr.on().name() + "/" + pr.number()
 
    message.withCorrelationId(cid).send()
 })
