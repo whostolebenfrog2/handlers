8cadb375d540adadbdcffcbba2dc980f236fefb8
David Dooling <david@atomist.com>
2017-04-04 15:58:44 -0500

    Add second build handler with default channel

--- .atomist/handlers/event/BuildStatus.ts
+++ .atomist/handlers/event/BuildStatus.ts
@@ -8,13 +8,11 @@ import {
     Respondable
 } from "@atomist/rug/operations/Handlers";
 import { Match } from "@atomist/rug/tree/PathExpression";
-import { ResponseHandler, EventHandler, Tags } from "@atomist/rug/operations/Decorators";
+import { ResponseHandler, EventHandler, Tags, Parameter } from "@atomist/rug/operations/Decorators";
 import { Build } from "@atomist/cortex/stub/Build";
 import * as mustache from "mustache";
 
-const channelId = "C4UC96BK5";
-
-@EventHandler("BuildStatusHandler", "Handle", "/Build()")
+@EventHandler("BuildStatusHandler", "Send build events to repo channel", "/Build()")
 @Tags("ci")
 export class BuildStatusHandler implements HandleEvent<Build, Build> {
     handle(event: Match<Build, Build>): Plan {
@@ -23,7 +21,6 @@ export class BuildStatusHandler implements HandleEvent<Build, Build> {
 
         let message;
         message = new Message(`new build? ${root.status()}`);
-        message.withChannelId(channelId);
         plan.add(message);
         let execute: Respondable<Execute> = {
             instruction: {
@@ -44,8 +41,52 @@ export class BuildStatusHandler implements HandleEvent<Build, Build> {
 
 export const buildStatusHandler = new BuildStatusHandler();
 
+@EventHandler("AllBuildStatusHandler", "Send all build events to a single channel", "/Build()")
+@Tags("ci")
+export class AllBuildStatusHandler implements HandleEvent<Build, Build> {
+    handle(event: Match<Build, Build>): Plan {
+        let root = event.root();
+        let plan: Plan = new Plan();
+
+        let message;
+        message = new Message(`new build? ${root.status()}`);
+        const channelId = "C4UC96BK5";
+        message.withChannelId(channelId);
+        plan.add(message);
+        let execute: Respondable<Execute> = {
+            instruction: {
+                name: "http",
+                kind: "execute",
+                parameters: {
+                    method: "get",
+                    url: `https://s3.amazonaws.com/static-files-for-public-consumption/lists/wins.txt`
+                }
+            },
+            onSuccess: {
+                kind: "respond",
+                name: "SendWinsImage",
+                parameters: { channelId: channelId }
+            }
+        }
+        plan.add(execute);
+
+        return plan;
+    }
+}
+
+export const allBuildStatusHandler = new AllBuildStatusHandler();
+
 @ResponseHandler("SendWinsImage", "Sends a wins image")
 class SendWinsResponder implements HandleResponse<any> {
+
+    @Parameter({
+        pattern: "^\\w+$",
+        minLength: 1,
+        maxLength: 200,
+        required: false
+    })
+    channelId: string;
+
 
     handle(response: Response<any>): Plan {
         let plan: Plan = new Plan();
@@ -65,7 +106,9 @@ class SendWinsResponder implements HandleResponse<any> {
   }]
 }`, { image: image, timestamp: new Date() });
         let message = new Message(body);
-        message.withChannelId(channelId);
+        if (this.channelId) {
+            message.withChannelId(this.channelId);
+        }
         plan.add(message);
         return plan;
     }
