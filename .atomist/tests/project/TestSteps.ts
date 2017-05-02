6222a1daa6c7cffee43b83f10d931f36b9132242
Sylvain Hellegouarch <sh@defuze.org>
2017-05-02 06:33:47 +0200

    adds a TypeScript Rug editor to a Rug project

---
+++ .atomist/tests/project/TestSteps.ts
@@ -0,0 +1,12 @@
+import { Project } from "@atomist/rug/model/Project";
+import { Given, ProjectScenarioWorld, Then, When } from "@atomist/rug/test/project/Core";
+
+When("the Test is run", (p, world) => {
+    const w = world as ProjectScenarioWorld;
+    const editor = w.editor("Test");
+    w.editWith(editor, { inputParameter: "the inputParameter value" });
+});
+
+Then("the hello file says hello", (p, world) => {
+    return p.fileContains("hello.txt", "Hello, World!");
+});
---
+++ .atomist/tests/project/TestTest.feature
@@ -0,0 +1,10 @@
+Feature: Make sure the sample TypeScript Editor has some tests
+  This is just a sample Gherkin feature file for the
+  Test.
+
+  Scenario: Test is added to your project by AddTest
+    Given the archive root
+    When the Test is run
+    Then parameters were valid
+    Then changes were made
+    Then the hello file says hello
